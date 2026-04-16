4bb86ca7490192213e81aca6f7ef9f55b04b30ee
JopsonDev <jopsonmason@outlook.com>
2026-04-16 12:47:42 -0400

    fixed thread.sleep

--- src/main/java/com/pluarlsight/ForLoop.java
+++ src/main/java/com/pluarlsight/ForLoop.java
@@ -3,9 +3,8 @@ package com.pluarlsight;
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
         for(int i = 10; i > 0; i--){
-            Thread thread = null;
-            thread.sleep(1000);
             System.out.println(i);
+            Thread.sleep(1000);
         }
         System.out.println("Launch!");
     }
