cacf00cbe2ed1f63397b78987c383f8f606a44b5
JopsonDev <jopsonmason@outlook.com>
2026-04-16 12:28:08 -0400

    fist push

---
+++ src/main/java/com/pluarlsight/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluarlsight;
+
+public class WhileLoop {
+    static void main() {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("I love Java");
+            i++; 
+        }
+    }
+}
